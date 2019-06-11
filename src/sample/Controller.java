3706c8dd7b638be44b3041cc0fa801bf81b31f7f
alexanderkalinowski <alk1842@thi.de>
2019-06-11 15:24:45 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Window;
+import javafx.scene.control.*;
+import com.leapmotion.leap.*;
+
+
+
+public class Controller {
+//FXML STUFF
+
+
+
+public void increaseVolume(){
+
+}
+
+
+
+}

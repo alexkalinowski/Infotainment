51b49021b65b56e7dbac2ce1fa00b1ff1b884c89
alexanderkalinowski <alk1842@thi.de>
2019-06-16 16:47:56 +0200

    first commit

--- src/application/FXMLDocumentController.java
+++ src/application/FXMLDocumentController.java
@@ -10,14 +10,53 @@ public class FXMLDocumentController {
 
     private Controller controller;
 
+
     @FXML
     private TextArea outputTextArea;
     @FXML
     private Slider volumeSlider;
+    @FXML
+    private ToggleButton playBtn;
+    @FXML
+    private Button prevSongBtn;
+    @FXML
+    private Button nextSongBtn;
+    @FXML
+    private Label onOffLabel;
+
+    private boolean infotainmentStatus;
+
+
+    public boolean isInfotainmentStatus() {
+        return this.infotainmentStatus;
+    }
+
+    public void setOn() {
+        this.infotainmentStatus = true;
+        volumeSlider.setValue(0);
+        volumeSlider.setDisable(false);
+        playBtn.setDisable(false);
+        prevSongBtn.setDisable(false);
+        nextSongBtn.setDisable(false);
+        onOffLabel.setText("ON");
+    }
+
+
+    public void setOff() {
+        this.infotainmentStatus = false;
+        volumeSlider.setValue(0);
+        volumeSlider.setDisable(true);
+        playBtn.setDisable(true);
+        nextSongBtn.setDisable(true);
+        prevSongBtn.setDisable(true);
+        onOffLabel.setText("OFF");
+    }
+
 
     @FXML
     private void initialize() {
         controller = new Controller();
+
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
@@ -31,24 +70,50 @@ public class FXMLDocumentController {
 
 
     public void refresh() {
-        System.out.println(controller.frame().fingers().get(1).tipPosition().getX());
+        System.out.println(controller.frame().hands().leftmost().palmNormal().pitch());
         setVolume();
+
+            powerOn();
+
+
+            powerOff();
+
 
 
     }
 
-    // Set volume via gesture in y-direction
+    // Set volume via gesture in y-direction with index finger
     public void setVolume() {
 
         if (controller.frame().fingers().get(1).isExtended() == true) {
             volumeSlider.setValue(controller.frame().fingers().get(1).tipPosition().getX());
         }
+
     }
 
-    //
+
+    // Turn on infotainment system via hand turn
+    public void powerOn() {
+        if(!isInfotainmentStatus()){
 
 
+            if (controller.frame().hands().leftmost().palmNormal().pitch() >= -0.05) {
+                setOn();
+            }
+        }
 
+    }
+
+    // Turn off infotainment system via hand turn in the opposite direction
+    public void powerOff() {
+        if (isInfotainmentStatus()) {
+
+            if (controller.frame().hands().leftmost().palmNormal().pitch() < -2.0) {
+                setOff();
+            }
+        }
+
+    }
 
 
 }
